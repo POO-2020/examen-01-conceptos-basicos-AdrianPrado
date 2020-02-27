71a3ee3a44d4656d1e4816cb4cb10ea8271c3df4
Macbook Air <macbookair@MacBook-Air-de-Macbook.local>
2020-02-26 18:52:09 -0600

    cantidad

---
+++ cantidad.js
@@ -0,0 +1,16 @@
+export default class Cantidad{
+    /**
+     * 
+     * @param {number} valor 
+     * @param {string} unidad 
+     */
+    constructor(valor, unidad)
+    {
+        this.valor = valor
+        this.unidad = unidad
+    }
+
+    getDescripcion(){
+        return (`${this.valor} ${this.unidad}`)
+    }
+}
